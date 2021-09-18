0c372522e9c86ebde0085ab4b673a15733e26131
Emrokker <90980951+Emrokker@users.noreply.github.com>
2021-09-18 20:14:55 +0200

    add java hello world

---
+++ java/HelloWorld.java
@@ -0,0 +1,14 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        String s = "Hello World!";
+        System.out.println(s);
+
+        boolean b = true;
+        if (b) {
+            int i = 3;
+            int j = 7;
+            var x = i + j;
+            System.out.print("Die Zahl ist " + x + ".");
+        }
+    }
+}
